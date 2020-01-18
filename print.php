ad691e234fdc1c1007bb9e6f0afc0200ee7cb846
kenil <kenilpadmani121@gmail.com>
2020-01-18 17:42:42 +0530

    Learing the print operator

---
+++ print.php
@@ -0,0 +1,24 @@
+<?php
+
+print("hello world<br>");
+
+print "hello<br>";
+
+if(print ("foo") && print ("bar")) {
+
+}
+
+$some_var ="true";
+($some_var) ? print 'true<br>' : print 'false<br>';
+
+$number=5;
+$name="kenil";
+
+print ($name.$number."<br>");
+
+
+$_test=print(5. ."kenil<br>");
+var_dump($_test);
+$_test1 = print("kenil". .5);
+var_dump($_test1);
+?>
